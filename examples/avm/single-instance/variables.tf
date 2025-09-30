53eb2f8eb6af61db7f006a9ea68646ce66d436dc
Adam Cain <adam@quadrivium.dev>
2025-09-30 03:49:48 +0100

    Add single instance AVM example

---
+++ examples/avm/single-instance/variables.tf
@@ -0,0 +1,90 @@
+variable "subscription_id" {
+  description = "The Subscription ID which should be used to deploy resources in to."
+  type        = string
+}
+
+variable "org" {
+  description = "Configuration details for the organisation, to be used for naming and tags for all resources created."
+  type = object({
+    name   = string
+    prefix = string
+  })
+  default = {
+    name   = "Quadrivium Cloud"
+    prefix = "qc"
+  }
+}
+
+variable "workload" {
+  description = "Configuration details for the workload being deployed."
+  type = object({
+    name       = string
+    short_name = string
+  })
+  default = {
+    name       = "CICD Single Instance"
+    short_name = "cicd-si"
+  }
+}
+
+variable "environment" {
+  description = "The environment for the deployment (e.g., dev, test, prod)."
+  type        = string
+  default     = "dev"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources into."
+  type = object({
+    name      = string
+    shortcode = string
+  })
+  default = {
+    name      = "UK South"
+    shortcode = "uks"
+  }
+}
+
+# -- Existing resource references --
+variable "resource_group_name" {
+  description = "The name of the resource group to use when looking up the existing resource group."
+  type        = string
+  default     = "rg-qc-cicd-single-instance-dev-uks-01"
+}
+
+variable "cicd_vnet_name" {
+  description = "The name of the CI/CD virtual network to use when looking up the existing virtual network."
+  type        = string
+  default     = "vnet-qc-cicd-single-instance-dev-uks-01"
+}
+
+variable "compute_subnet_name" {
+  description = "The name of the compute subnet to use when looking up the existing subnet."
+  type        = string
+  default     = "snet-compute-dev-uks-01"
+}
+
+# -- Key Vault variables --
+variable "user_ip_address" {
+  description = "Your public IP address in CIDR notation."
+  type        = string
+}
+
+variable "key_vault_ip_rules" {
+  description = "List of IP addresses or CIDR ranges that are allowed to access the Key Vault."
+  type        = list(string)
+  default     = []
+}
+
+# -- Virtual Machine variables --
+variable "admin_username" {
+  description = "The admin username for the Virtual Machine."
+  type        = string
+  default     = "azureadmin"
+}
+
+variable "vm_size" {
+  description = "The size of the Virtual Machine."
+  type        = string
+  default     = "Standard_B2s"
+}
---
+++ examples/avm/single-instance/virtual_machines.linux.tf
@@ -0,0 +1,84 @@
+# Create an SSH public key for the admin user
+resource "tls_private_key" "admin_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Upload the SSH public key to Azure
+module "avm_ssh_public_key" {
+  source  = "Azure/avm-res-compute-sshpublickey/azurerm"
+  version = "0.1.0"
+
+  name                = replace("sshkey${local.resource_name_prefix}linux${local.resource_name_suffix}01", "-", "") # "sshkeyqccicdsingleinstancelinuxdevuks01"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  public_key          = tls_private_key.admin_ssh_key.public_key_openssh
+}
+
+# Save the SSH private key in a Key Vault secret
+resource "azurerm_key_vault_secret" "admin_ssh_key" {
+  name         = "${var.workload.short_name}-vm-admin-ssh-key" # "cicd-si-vm-admin-ssh-key"
+  value        = tls_private_key.admin_ssh_key.private_key_pem
+  key_vault_id = module.avm_key_vault.resource_id
+
+  # Ensure Key Vault is created before adding the secret and removes the secret first if the Key Vault is deleted
+  depends_on = [azurerm_key_vault_access_policy.current_user]
+}
+
+# Create a Public IP for the VM
+module "avm_linux_pip" {
+  source  = "Azure/avm-res-network-publicipaddress/azurerm"
+  version = "0.2.0"
+
+  name                = "pip-${local.resource_name_prefix}-linux-${local.resource_name_suffix}-01" # "pip-qc-cicd-linux-dev-uks-01"
+  location            = var.location.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Create a Linux virtual machine
+
+module "avm_linux_vm" {
+  source  = "Azure/avm-res-compute-virtualmachine/azurerm"
+  version = "0.19.3"
+
+  name                = "vm-${local.resource_name_prefix}-linux-${local.resource_name_suffix}-01" # "vm-qc-cicd-linux-dev-uks-01"
+  location            = var.location.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  os_type                    = "Linux"
+  sku_size                   = var.vm_size # "Standard_B2s"
+  zone                       = "1"
+  encryption_at_host_enabled = false
+
+  network_interfaces = {
+    nic0 = {
+      name = "nic-${local.resource_name_prefix}-linux-${local.resource_name_suffix}-01" # "nic-qc-cicd-linux-dev-uks-01"
+      ip_configurations = {
+        ipconfig0 = {
+          name                          = "ipconfig"
+          private_ip_subnet_resource_id = data.azurerm_subnet.compute.id
+          public_ip_address_resource_id = module.avm_linux_pip.resource_id
+        }
+      }
+    }
+  }
+
+  account_credentials = {
+    admin_credentials = {
+      username                           = var.admin_username
+      ssh_keys                           = [tls_private_key.admin_ssh_key.public_key_openssh]
+      generate_admin_password_or_ssh_key = false
+    }
+  }
+
+  os_disk = {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference = {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
